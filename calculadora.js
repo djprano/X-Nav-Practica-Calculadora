4df175030b8c950dbdd3820224e5e7995c382cde
djprano <djprano@gmail.com>
2015-03-03 12:49:41 +0100

    añadido soporte a teclado y redondeo para no salirme de la pantalla

--- calculadora.js
+++ calculadora.js
@@ -3,9 +3,9 @@ var operador = 0.0;
 var operacion = '';
 
 function redondeo(numero, decimales){
-var flotante = parseFloat(numero);
-var resultado = Math.round(flotante*Math.pow(10,decimales))/Math.pow(10,decimales);
-return resultado;
+	var flotante = parseFloat(numero);
+	var resultado = Math.round(flotante*Math.pow(10,decimales))/Math.pow(10,decimales);
+	return resultado;
 }
 
 function eval (num , ope){
@@ -32,7 +32,7 @@ function eval (num , ope){
 		break;
 
 		case'%':
-		operador=operador/100;
+		operador=redondeo(num/100,10);
 		$('#resultado').text(operador.toString());
 		break;
 
@@ -44,7 +44,7 @@ jQuery(document).ready(function(){
 	$('div#result').text(redondeo(operador,10).toString());
 	//actualizamos result
 	$(document).click(function(){
-		$('div#result').text(operador.toString());
+		$('div#result').text(redondeo(operador,10).toString());
 	})
 	$('span').mousedown(function(){
 		//sombra
@@ -93,8 +93,7 @@ jQuery(document).ready(function(){
 				$('#resultado').text('');
 				operacion = $(this).text();
 			}else{//porcentajes
-				var a = parseFloat($('#resultado').text())/100;
-				$('#resultado').text(a.toString());
+				eval($('#resultado').text(),$(this).text());
 			}
 		}
 
@@ -113,9 +112,190 @@ jQuery(document).ready(function(){
 		}
 		
 	});
-	$('span').mouseup(function(){
-		if($(this).hasClass('numeros')||$(this).hasClass('cero')||$(this).hasClass('operador')||$(this).hasClass('ac')){
-			$(this).removeClass('press');
-		}
-	})
+$('span').mouseup(function(){
+	if($(this).hasClass('numeros')||$(this).hasClass('cero')||$(this).hasClass('operador')||$(this).hasClass('ac')){
+		$(this).removeClass('press');
+	}
+})
+
+$(document).keydown(function(event){
+	//alert(event.keyCode);
+	switch(event.keyCode) {
+		case 49:
+		$('span:contains("1")').mousedown();
+		break;
+		case 50:
+		$('span:contains("2")').mousedown();
+		break;
+		case 51:
+		$('span:contains("3")').mousedown();
+		break;
+		case 52:
+		$('span:contains("4")').mousedown();
+		break;
+		case 53:
+		$('span:contains("5")').mousedown();
+		break;
+		case 54:
+		$('span:contains("6")').mousedown();
+		break;
+		case 55:
+		$('span:contains("7")').mousedown();
+		break;
+		case 56:
+		$('span:contains("8")').mousedown();
+		break;
+		case 57:
+		$('span:contains("9")').mousedown();
+		break;
+		case 96:
+		$('span:contains("0")').mousedown();
+		break;
+		case 97:
+		$('span:contains("1")').mousedown();
+		break;
+		case 98:
+		$('span:contains("2")').mousedown();
+		break;
+		case 99:
+		$('span:contains("3")').mousedown();
+		break;
+		case 100:
+		$('span:contains("4")').mousedown();
+		break;
+		case 101:
+		$('span:contains("5")').mousedown();
+		break;
+		case 102:
+		$('span:contains("6")').mousedown();
+		break;
+		case 103:
+		$('span:contains("7")').mousedown();
+		break;
+		case 104:
+		$('span:contains("8")').mousedown();
+		break;
+		case 105:
+		$('span:contains("9")').mousedown();
+		break;
+		case 96:
+		$('span:contains("0")').mousedown();
+		break;
+		case 110:
+		$('span:contains(".")').mousedown();
+		break;
+		case 8:
+		$('span:contains("C")').mousedown();
+		event.preventDefault();
+		return false;
+		break;
+		case 13:
+		$('span:contains("=")').mousedown();
+		break;
+		case 107:
+		$('span:contains("+")').mousedown();
+		break;
+		case 109:
+		$('span:contains("-")').mousedown();
+		break;
+		case 106:
+		$('span:contains("\u00D7")').mousedown();
+		break;
+		case 111:
+		$('span:contains("\u00F7")').mousedown();
+		break;
+
+
+
+
+	}
+});
+
+$(document).keyup(function(event){
+	$('div#result').text(redondeo(operador,10).toString());
+	switch(event.keyCode) {
+		case 49:
+		$('span:contains("1")').mouseup();
+		break;
+		case 50:
+		$('span:contains("2")').mouseup();
+		break;
+		case 51:
+		$('span:contains("3")').mouseup();
+		break;
+		case 52:
+		$('span:contains("4")').mouseup();
+		break;
+		case 53:
+		$('span:contains("5")').mouseup();
+		break;
+		case 54:
+		$('span:contains("6")').mouseup();
+		break;
+		case 55:
+		$('span:contains("7")').mouseup();
+		break;
+		case 56:
+		$('span:contains("8")').mouseup();
+		break;
+		case 57:
+		$('span:contains("9")').mouseup();
+		break;
+		case 96:
+		$('span:contains("0")').mouseup();
+		break;
+		case 97:
+		$('span:contains("1")').mouseup();
+		break;
+		case 98:
+		$('span:contains("2")').mouseup();
+		break;
+		case 99:
+		$('span:contains("3")').mouseup();
+		break;
+		case 100:
+		$('span:contains("4")').mouseup();
+		break;
+		case 101:
+		$('span:contains("5")').mouseup();
+		break;
+		case 102:
+		$('span:contains("6")').mouseup();
+		break;
+		case 103:
+		$('span:contains("7")').mouseup();
+		break;
+		case 104:
+		$('span:contains("8")').mouseup();
+		break;
+		case 105:
+		$('span:contains("9")').mouseup();
+		break;
+		case 96:
+		$('span:contains("0")').mouseup();
+		break;
+		case 110:
+		$('span:contains(".")').mouseup();
+		break;
+		case 8:
+		$('span:contains("C")').mouseup();
+		break;
+		case 13:
+		$('span:contains("=")').mouseup();
+		break;
+		case 107:
+		$('span:contains("+")').mouseup();
+		break;
+		case 109:
+		$('span:contains("-")').mouseup();
+		break;
+		case 106:
+		$('span:contains("\u00D7")').mouseup();
+		break;
+		case 111:
+		$('span:contains("\u00F7")').mouseup();
+		break;
+
+	}
+});
 });
